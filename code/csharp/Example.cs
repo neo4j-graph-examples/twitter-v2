05c2131b3feb75d0f5900ff8549fd49b31619356
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2021-03-11 09:43:13 +0000

    Triggered by direct commit. Origin: https://github.com/neo4j-contrib/sandbox-code-updater/commit/c796422eca1c507117a47acfde6e4086c34a5b84

---
+++ code/csharp/Example.cs
@@ -0,0 +1,38 @@
+// install dotnet core on your system
+// dotnet new console -o .
+// dotnet add package Neo4j.Driver
+// paste in this code into Program.cs
+// dotnet run
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Neo4j.Driver;
+  
+namespace dotnet {
+  class Example {
+  static async Task Main() {
+    var driver = GraphDatabase.Driver("bolt://<HOST>:<BOLTPORT>", 
+                    AuthTokens.Basic("<USERNAME>", "<PASSWORD>"));
+
+    var cypherQuery =
+      @"
+      MATCH (u:User {screen_name: $screenName})<-[r:MENTIONS]-(t:Tweet)-[r2:TAGS]->(h:Hashtag)
+      RETURN h.name as hashtag
+      ";
+
+    var session = driver.AsyncSession(o => o.WithDatabase("neo4j"));
+    var result = await session.ReadTransactionAsync(async tx => {
+      var r = await tx.RunAsync(cypherQuery, 
+              new { screenName="NASA"});
+      return await r.ToListAsync();
+    });
+
+    await session?.CloseAsync();
+    foreach (var row in result)
+      Console.WriteLine(row["hashtag"].As<string>());
+	  
+    }
+  }
+}
