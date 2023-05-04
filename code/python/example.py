a5585674d94e0bae6a0fd12b3bc41b3a4803502a
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2023-05-04 10:23:05 +0000

    Triggered by dispatch event. Origin: https://github.com/neo4j-graph-examples/template/commit/8556d8f9aeed10508116ebd1d7a02b9694891cff

--- code/python/example.py
+++ code/python/example.py
@@ -1,22 +1,20 @@
-# pip3 install neo4j-driver
+# pip3 install neo4j
 # python3 example.py
 
 from neo4j import GraphDatabase, basic_auth
-
-driver = GraphDatabase.driver(
-  "bolt://<HOST>:<BOLTPORT>",
-  auth=basic_auth("<USERNAME>", "<PASSWORD>"))
 
 cypher_query = '''
 MATCH (u:User {screen_name: $screenName})<-[r:MENTIONS]-(t:Tweet)-[r2:TAGS]->(h:Hashtag)
 RETURN h.name as hashtag
 '''
 
-with driver.session(database="neo4j") as session:
-  results = session.read_transaction(
-    lambda tx: tx.run(cypher_query,
-                      screenName="NASA").data())
-  for record in results:
-    print(record['hashtag'])
-
-driver.close()
+with GraphDatabase.driver(
+    "neo4j://<HOST>:<BOLTPORT>",
+    auth=("<USERNAME>", "<PASSWORD>")
+) as driver:
+    result = driver.execute_query(
+        cypher_query,
+        screenName="NASA",
+        database_="neo4j")
+    for record in result.records:
+        print(record['hashtag'])
